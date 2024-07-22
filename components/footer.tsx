1cc4442e6c31e822349f1f45d190fd0d88f705b3
jocarino <joao.goncalo.veloso@gmail.com>
2024-07-22 20:59:04 +0100

    feat:footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,16 +12,16 @@ export default function Footer() {
   };
   return (
     <footer className="text-center items-center align-middle flex flex-row w-full">
-      <div className="ml-[20rem] px-4  flex flex-row justify-between items-center bg-gradient-to-r from-white to-pink-200 w-full h-[80px]">
+      <div className="sm:ml-[20rem] text-[10px] sm:text-base px-4  flex flex-row justify-between items-center bg-gradient-to-r from-white to-pink-200 w-full h-[80px]">
         <p className="text-pink-400 font-medium">
           Damola Kevwe | Designer | damolakevwe@gmail.com
         </p>
         <div className="flex items-center space-x-4">
           <Link href="https://www.linkedin.com/in/oluwadamola-olaleye-29b10279/">
-            <FaLinkedin className="w-6 h-6 text-pink-400" />
+            <FaLinkedin className="sm:w-6 sm:h-6 w-5 h-5 text-pink-400" />
           </Link>
           <a href={`mailto:${email}`} onClick={handleEmailClick}>
-            <HiMail className="w-6 h-6 text-pink-400" />
+            <HiMail className="sm:w-6 sm:h-6 w-5 h-5 text-pink-400" />
           </a>
         </div>
       </div>
--- components/hoverButton.tsx
+++ components/hoverButton.tsx
@@ -12,7 +12,7 @@ export default function FloatingButton({ onClick, }: FloatinButtonProps) {
     return (
         <button
             onClick={onClick}
-            className="fixed bottom-4 right-4 z-50 p-4 bg-pink-400 text-white rounded-full shadow-lg"
+            className="fixed bottom-24 right-4 z-50 p-4 bg-pink-400 text-white rounded-full shadow-lg"
         >
             <HiMenu className="w-6 h-6" />
         </button>
