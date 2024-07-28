f1cead45664b0543496f5f257f003d07b773e780
jocarino <joao.goncalo.veloso@gmail.com>
2024-07-28 14:26:22 +0100

    feat: adding caddy

--- components/landing.tsx
+++ components/landing.tsx
@@ -7,59 +7,53 @@ import { useActiveSectionContext } from "@/context/active-section-context";
 import Head from "next/head";
 
 export default function Landing() {
-    const { setActiveSection, } = useActiveSectionContext();
-    React.useEffect(() => {
-        setActiveSection(null);
-    }, [setActiveSection]);
-    return (
-        <>
-            <Head>
-                <link
-                    rel="preload"
-                    href="/dk.png"
-                    as="image"
-                />
-            </Head>
-            <section
-                className="mb-28 max-w-[90rem] text-center lg:mb-0 scroll-mt-[100rem]"
-            >
-                <div className="flex flex-col-reverse lg:flex-row items-center justify-center">
-                    <div className="flex flex-col max-w-[50rem] space-y-3 lg:space-y-6">
-                        <h1
-                            className="text-center lg:text-left mb-5 lg:mb-10 mt-4 px-4 text-4xl lg:text-[64px] lg:leading-[80px] lg:text-4xl space-y-3 lg:space-y-6"
-                        >
-                            <p className="">Hi!👋🏾</p>
-                            <p className="text-left">I’m{" "}<span className="text-pink-400">Damola Kevwe</span></p>
-                            <p className="text-left">I design & create engaging journeys that build meaningful experiences</p>
-                        </h1>
-                        <div
-                            className="flex flex-col lg:flex-row items-start justify-start gap-2 px-4 text-lg font-medium"
-                        >
-                            <Link
-                                href="/work"
-                                className="group tracking-wide bg-pink-400 text-white font-bold text-2xl lg:text-[40px] lg:leading-[50px] px-6 py-4 flex items-center gap-2 rounded-2xl outline-none hover:scale-110 hover:bg-pink-900 transition"
-                                prefetch={true}
-                            >
-                                Check out my work
-                            </Link>
-                        </div>
-                    </div>
-                    <div className="flex items-center justify-center lg:ml-[-8rem]">
-                        <Image
-                            src="/dk.png"
-                            alt="Damola's portrait"
-                            width="549"
-                            height="549"
-                            quality="85"
-                            priority={true}
-                            className="rounded-full w-48 lg:w-full object-cover shadow-none"
-                            placeholder="blur"
-                            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=="
-                        />
-                    </div>
-                </div>
-
-            </section >
-        </>
-    );
+  const { setActiveSection } = useActiveSectionContext();
+  React.useEffect(() => {
+    setActiveSection(null);
+  }, [setActiveSection]);
+  return (
+    <>
+      <Head>
+        <link rel="preload" href="/dk.png" as="image" />
+      </Head>
+      <section className="mb-28 max-w-[90rem] text-center lg:mb-0 scroll-mt-[100rem]">
+        <div className="flex flex-col-reverse lg:flex-row items-center justify-center">
+          <div className="flex flex-col max-w-[60rem] space-y-3 lg:space-y-6">
+            <h1 className="text-center lg:text-left mb-5 lg:mb-10 mt-4 px-4 text-4xl md:text-5xl lg:text-6xl lg:leading-[5rem] space-y-3 lg:space-y-6">
+              <p className="">Hi!👋🏾</p>
+              <p className="text-left">
+                I’m <span className="text-pink-400">Damola Kevwe</span>
+              </p>
+              <p className="text-left">
+                I design & create engaging journeys that build meaningful
+                experiences
+              </p>
+            </h1>
+            <div className="flex flex-col lg:flex-row items-start justify-start gap-2 px-4 text-lg font-medium">
+              <Link
+                href="/work"
+                className="group tracking-wide bg-pink-400 text-white font-bold text-2xl lg:text-[40px] lg:leading-[50px] px-6 py-4 flex items-center gap-2 rounded-2xl outline-none hover:scale-110 hover:bg-pink-900 transition"
+                prefetch={true}
+              >
+                Check out my work
+              </Link>
+            </div>
+          </div>
+          <div className="flex items-center justify-center lg:ml-[-7rem] lg:min-w-[30rem]">
+            <Image
+              src="/dk.png"
+              alt="Damola's portrait"
+              width="549"
+              height="549"
+              quality="85"
+              priority={true}
+              className="rounded-full w-48 lg:w-full object-cover shadow-none"
+              placeholder="blur"
+              blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=="
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
