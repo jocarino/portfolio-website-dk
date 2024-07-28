28072937f417ba9e5f988daea5bca24961f4e77e
jocarino <joao.goncalo.veloso@gmail.com>
2024-07-28 14:46:59 +0100

    fix: landing pages fixes

--- components/landing.tsx
+++ components/landing.tsx
@@ -17,9 +17,9 @@ export default function Landing() {
         <link rel="preload" href="/dk.png" as="image" />
       </Head>
       <section className="mb-28 max-w-[90rem] text-center lg:mb-0 scroll-mt-[100rem]">
-        <div className="flex flex-col-reverse lg:flex-row items-center justify-center">
+        <div className="flex flex-col-reverse lg:flex-row items-center justify-center lg:p-10">
           <div className="flex flex-col max-w-[60rem] space-y-3 lg:space-y-6">
-            <h1 className="text-center lg:text-left mb-5 lg:mb-10 mt-4 px-4 text-4xl md:text-5xl lg:text-6xl lg:leading-[5rem] space-y-3 lg:space-y-6">
+            <h1 className="text-center lg:text-left mb-5 lg:mb-10 mt-4 px-4 text-4xl lg:text-5xl lg:leading-[4rem] space-y-3 lg:space-y-6">
               <p className="">Hi!👋🏾</p>
               <p className="text-left">
                 I’m <span className="text-pink-400">Damola Kevwe</span>
@@ -32,7 +32,7 @@ export default function Landing() {
             <div className="flex flex-col lg:flex-row items-start justify-start gap-2 px-4 text-lg font-medium">
               <Link
                 href="/work"
-                className="group tracking-wide bg-pink-400 text-white font-bold text-2xl lg:text-[40px] lg:leading-[50px] px-6 py-4 flex items-center gap-2 rounded-2xl outline-none hover:scale-110 hover:bg-pink-900 transition"
+                className="group tracking-wide bg-pink-400 text-white font-bold text-2xl lg:text-3xl lg:leading-[3rem] px-6 py-4 flex items-center gap-2 rounded-2xl outline-none hover:scale-110 hover:bg-pink-900 transition"
                 prefetch={true}
               >
                 Check out my work
@@ -47,7 +47,7 @@ export default function Landing() {
               height="549"
               quality="85"
               priority={true}
-              className="rounded-full w-48 lg:w-full object-cover shadow-none"
+              className="rounded-full w-36 lg:w-full object-cover shadow-none"
               placeholder="blur"
               blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=="
             />
