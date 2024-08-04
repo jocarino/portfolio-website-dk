561394b58379692a1f255138d9019da0e5038131
jocarino <joao.goncalo.veloso@gmail.com>
2024-08-04 16:46:32 +0100

    fix: only on mobile

--- components/title.tsx
+++ components/title.tsx
@@ -12,7 +12,9 @@ export default function Title({
   return (
     <h3
       className={`text-2xl lg:text-3xl font-bold capitalize mb-2 text-left ${
-        variant === "about" ? "bg-pink-200/70 rounded-lg p-2" : ""
+        variant === "about"
+          ? "bg-pink-200/70 rounded-lg p-2 lg:bg-transparent lg:rounded-none lg:p-0"
+          : ""
       }`}
     >
       {children}
